637ca2bcaf0730c531ff0a1dbc84fce24ebf2642
diek <diek@bellaliant.net>
2015-02-24 22:44:51 -0400

    more stuff

--- _javascript1/scope.js
+++ _javascript1/scope.js
@@ -2,10 +2,25 @@
 
  function init() {
   // your code here to set up a button click handler
-  // button clicked, call pythagoras()
     var button = document.getElementById("submit");
-    button.onclick = pythagoras;
+    button.onclick = circleArea;
  }
 
 var circleRadius = 3;
 alert("Radius: " + circleRadius);
+
+var area = circleArea();
+
+function circleArea(){
+  var circleRadius = 5;
+  var area = Math.PI * Math.pow(circleRadius, 2);
+  displayRadius();
+  return area;
+}
+
+function displayRadius(){
+  var div = document.getElementById("result");
+  div.innerHTML = "Radius in the function circleArea(): "; //+ circleRadius;
+}
+
+

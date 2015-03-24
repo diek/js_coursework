1e5e0d4d20073008318049a66fb6ffbfb29cd495
diek <diek@bellaliant.net>
2015-03-24 11:22:46 -0300

    updates to part 15

--- _javascript1/15_elm_attributes_style/_script/photos_1.5.js
+++ _javascript1/15_elm_attributes_style/_script/photos_1.5.js
@@ -10,19 +10,29 @@ function init() {
     oceanSpan.onclick = selectOcean;
 }
 
-function selectPets() {
+function selectPets(){
     var ul = document.getElementById("petsList");
-
+    showHide(ul);
 }
 
-function selectLandscape() {
+function selectLandscape(){
     var ul = document.getElementById("landscapeList");
-
+    showHide(ul);
 }
 
-function selectOcean() {
+function selectOcean(){
     var ul = document.getElementById("oceanList");
-
+    showHide(ul);
 }
 
-
+function showHide(el){
+    var ulClass = el.getAttribute("class");
+    if (ulClass == "show"){
+        // item is selected, so de-select
+        el.setAttribute("class", "");
+    }
+    else {
+        // item is not selected, so select it
+        el.setAttribute("class", "show");
+    }
+}

149688c58355a86b4ecce05fda3feae5d86bb258
diek <diek@bellaliant.net>
2015-04-08 16:41:48 -0300

    more ajax

---
+++ _JSON_Ajax/02_first_ajax_app/pets.js
@@ -0,0 +1,28 @@
+window.onload = init;
+
+function init() {
+  getPetData();
+}
+
+function getPetData(){
+  var request = new XMLHttpRequest();
+
+  //request.open('GET', 'http://127.0.0.1:5000/json_data');
+  request.open('GET', 'pets.json');
+  request.onreadystatechange = function(){
+    var div = document.getElementById('pets');
+    if (this.readyState == this.DONE && this.status == 200){
+      var type = request.getResponseHeader('Content-Type');
+      console.log('Content-Type ' + type);
+      console.log('Status: ' + this.StatusText);
+      if (this.responseText != null){
+        div.innerHTML = this.responseText;
+      }
+      else{
+        div.innerHTML = 'Error: No Data.';
+      }
+    }
+  };
+
+  request.send();
+}
